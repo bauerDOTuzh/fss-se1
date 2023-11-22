a3a7e41613be9ca8a7ddde65671eb7189379deb2
AlessioBrazerol <alessio.bra@hotmail.ch>
2023-11-22 19:48:22 +0100

    Updated ex_3_AB_V2.py

--- ex_3_AB_V2.py
+++ ex_3_AB_V2.py
@@ -13,7 +13,7 @@ from pydriller import Repository, Git
 from tqdm import tqdm
 
 from timeit import default_timer
-from scipy.sparse import csc_matrix
+from scipy.sparse import csc_matrix, vstack
 
 
 def create_commit_vector(commit, files):
@@ -26,14 +26,14 @@ def create_commit_vector(commit, files):
             v[0][index] = 1
     v_sparse = csc_matrix(v)
     return {"time": commit.committer_date,
-            "modifications": v_sparse,
-            "modificationsT": v_sparse.transpose(copy=True)}
+            "modifications": v_sparse}
 
-def convert_to_format(result_dict, matrix, idx):
+
+def convert_to_format(result_dict, matrix, idx, debug):
     for row, col in zip(*matrix.nonzero()):
         if row == col:
             continue
-        val = temporal_coupling_matrix_24h[row, col]
+        val = matrix[row, col]
         key = str(row) + "_" + str(col)
         if key not in result_dict:
             result_dict[key] = {"file_pair": [entities[row], entities[col]],
@@ -55,8 +55,9 @@ def convert_to_format(result_dict, matrix, idx):
                                             "commit_count": 0
                                         }
                                     ]}
-            result_dict[key]["coupled_commits"][idx]["commit_count"] = int(val)
-
+        else:
+            pass
+        result_dict[key]["coupled_commits"][idx]["commit_count"] = int(val)
 
 
 if __name__ == '__main__':
@@ -70,17 +71,22 @@ if __name__ == '__main__':
 
     numberOfFiles = len(entities)
 
-    temporal_coupling_matrix_24h = csc_matrix((numberOfFiles, numberOfFiles), dtype=int)
-    temporal_coupling_matrix_48h = csc_matrix((numberOfFiles, numberOfFiles), dtype=int)
-    temporal_coupling_matrix_72h = csc_matrix((numberOfFiles, numberOfFiles), dtype=int)
-    temporal_coupling_matrix_168h = csc_matrix((numberOfFiles, numberOfFiles), dtype=int)
-
     commits = [c for c in Repository(path).traverse_commits()]
 
     global_progress = tqdm(unit="commit", total=len(commits), desc="Computing temporal coupling matrix", position=0,
                            leave=False)
 
     time_frame = deque()
+
+    avgLength = 0
+    update_v_24 = []
+    update_w_24 = []
+    update_v_48 = []
+    update_w_48 = []
+    update_v_72 = []
+    update_w_72 = []
+    update_v_168 = []
+    update_w_168 = []
 
     for commit in commits:
         c1 = create_commit_vector(commit, entities)
@@ -89,24 +95,28 @@ if __name__ == '__main__':
         inside48Window = False
         inside24Window = False
         i = 0
+        avgLength += 1 / len(commits) * len(time_frame)
         while i < len(time_frame):
             c2 = time_frame[i]
             if not inside24Window:
                 delta = c1["time"] - c2["time"]
             if inside168Window or delta <= datetime.timedelta(hours=168):
-                f1T = c1["modificationsT"]
+                f1 = c1["modifications"]
                 f2 = c2["modifications"]
-                update_matrix = f1T @ f2
-                temporal_coupling_matrix_168h += update_matrix
+                update_v_168.append(f1)
+                update_w_168.append(f2)
                 inside168Window = True
                 if inside72Window or delta <= datetime.timedelta(hours=72):
-                    temporal_coupling_matrix_72h += update_matrix
+                    update_v_72.append(f1)
+                    update_w_72.append(f2)
                     inside72Window = True
                     if inside48Window or delta <= datetime.timedelta(hours=48):
-                        temporal_coupling_matrix_48h += update_matrix
+                        update_v_48.append(f1)
+                        update_w_48.append(f2)
                         inside48Window = True
                         if inside24Window or delta <= datetime.timedelta(hours=24):
-                            temporal_coupling_matrix_24h += update_matrix
+                            update_v_24.append(f1)
+                            update_w_24.append(f2)
                             inside24Window = True
                 i += 1
             else:
@@ -117,12 +127,38 @@ if __name__ == '__main__':
 
     global_progress.close()
 
+    math_total = 4
+    math_progress = tqdm(unit="commit", total=math_total, desc="Computing temporal coupling matrix", position=0,
+                           leave=False)
+
+    v_matrix24 = vstack(update_v_24)
+    w_matrix24 = vstack(update_w_24)
+    v_matrix48 = vstack(update_v_48)
+    w_matrix48 = vstack(update_w_48)
+    v_matrix72 = vstack(update_v_72)
+    w_matrix72 = vstack(update_w_72)
+    v_matrix168 = vstack(update_v_168)
+    w_matrix168 = vstack(update_w_168)
+    temporal_coupling_matrix_24h = v_matrix24.transpose() @ w_matrix24
+    math_progress.update(1)
+    temporal_coupling_matrix_48h = v_matrix48.transpose() @ w_matrix48
+    math_progress.update(1)
+    temporal_coupling_matrix_72h = v_matrix72.transpose() @ w_matrix72
+    math_progress.update(1)
+    temporal_coupling_matrix_168h = v_matrix168.transpose() @ w_matrix168
+    math_progress.update(1)
+
+    math_progress.close()
+
+    print("avg: {}".format(avgLength))
+
     temporal_coupling = {}
 
-    convert_to_format(temporal_coupling, temporal_coupling_matrix_24h, 0)
-    convert_to_format(temporal_coupling, temporal_coupling_matrix_48h, 1)
-    convert_to_format(temporal_coupling, temporal_coupling_matrix_72h, 2)
-    convert_to_format(temporal_coupling, temporal_coupling_matrix_168h, 3)
+    convert_to_format(temporal_coupling, temporal_coupling_matrix_24h, 0, "24")
+    convert_to_format(temporal_coupling, temporal_coupling_matrix_48h, 1, "48")
+    convert_to_format(temporal_coupling, temporal_coupling_matrix_72h, 2, "72")
+
+    convert_to_format(temporal_coupling, temporal_coupling_matrix_168h, 3, "168")
 
     print("saving")
 
